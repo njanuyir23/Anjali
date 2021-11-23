f3749df9739fe2507899b59b38d6454219a77a87
njanuyir23 <94900617+njanuyir23@users.noreply.github.com>
2021-11-23 18:43:25 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2020 Rafhan Ser 
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhatsAsena - Rafhan Ser 
 */
 
 const fs = require("fs");
